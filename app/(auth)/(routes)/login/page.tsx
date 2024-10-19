08e6d2733f4be39d21567db9d0f93953d877415f
Kedar-Khati <khatikm_1@rknec.edu>
2024-10-19 23:29:31 +0530

    Created Proper Folder Structure

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div className="bg-yellow-100">
+            Login Page!
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+    return (
+        <div className="bg-yellow-100 content">
+            Register Page
+        </div>
+    );
+}
+
+export default RegisterPage;
